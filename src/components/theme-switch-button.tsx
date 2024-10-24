837ddddfe45765196ded7af47ab0770ff4bd6c6c
Roby Codes <robert.cfx@proton.me>
2024-10-25 00:58:32 +0200

    feat(component): created theme switch button component.

---
+++ src/components/theme-switch-button.tsx
@@ -0,0 +1,7 @@
+import { MdOutlineWbSunny } from "react-icons/md";
+
+export const ThemeSwitchButton = () => (
+  <span className="fixed bottom-6 right-6 z-[999] size-12">
+    <MdOutlineWbSunny />
+  </span>
+);
