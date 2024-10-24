474d8143f3eadb3e512c7ee54c2428b2e3ccc448
Roby Codes <robert.cfx@proton.me>
2024-10-25 01:21:30 +0200

    style(layout): added mono font to project.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${appFont.className} bg-slate-50 antialiased`}>
+      <body
+        className={`${appFont.className} bg-slate-50 font-mono antialiased`}
+      >
         <ThemeContextProvider>
           {children}
           <ThemeSwitchButton />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,13 @@ import { Calculator } from "@/components/calculator";
 
 const HomePage = () => (
   <main>
-    <section className="relative flex min-h-screen items-center justify-center overflow-hidden">
-      <Calculator />
+    <section className="flex min-h-screen items-center justify-center">
+      <div className="container flex max-w-md flex-col space-y-4 px-4">
+        <h1 className="text-center text-xl font-bold uppercase">
+          Trade Size Calculator
+        </h1>
+        <Calculator />
+      </div>
     </section>
   </main>
 );
