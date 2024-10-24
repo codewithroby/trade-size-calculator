8326e807a3440c4ce2afcb165d4f4aacf43b656a
Roby Codes <robert.cfx@proton.me>
2024-10-25 00:59:13 +0200

    style(layout): added theme switch button component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import ThemeContextProvider from "@/context/theme-context";
+import { ThemeSwitchButton } from "@/components/theme-switch-button";
 
 const appFont = Inter({
   subsets: ["latin"],
@@ -23,7 +24,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${appFont.className} bg-slate-50 antialiased`}>
-        <ThemeContextProvider>{children}</ThemeContextProvider>
+        <ThemeContextProvider>
+          {children}
+          <ThemeSwitchButton />
+        </ThemeContextProvider>
       </body>
     </html>
   );
