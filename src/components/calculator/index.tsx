76485aef2e87bc6fd4bc0e7dc02be933ca33d3f9
Roby Codes <robert.cfx@proton.me>
2024-10-25 01:27:38 +0200

    style(calculator): completed base layout.

--- src/components/calculator/index.tsx
+++ src/components/calculator/index.tsx
@@ -1,8 +1,10 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Forex } from "@/components/calculator/forex";
+import { Crypto } from "@/components/calculator/crypto";
 
 export const Calculator = () => (
   <div className="rounded-md bg-white p-6 shadow-sm">
-    <Tabs defaultValue="account" className="w-full">
+    <Tabs defaultValue="forex" className="w-full">
       <TabsList className="w-full">
         <TabsTrigger value="forex" className="flex-1 uppercase">
           Forex
@@ -12,9 +14,11 @@ export const Calculator = () => (
         </TabsTrigger>
       </TabsList>
       <TabsContent value="forex">
-        Make changes to your account here.
+        <Forex />
       </TabsContent>
-      <TabsContent value="crypto">Change your password here.</TabsContent>
+      <TabsContent value="crypto">
+        <Crypto />
+      </TabsContent>
     </Tabs>
   </div>
 );
