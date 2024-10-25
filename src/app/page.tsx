4cccb60de7ee0c6ca61a76b0903ddc4cbcad556c
Roby Codes <robert.cfx@proton.me>
2024-10-25 02:18:28 +0200

    style(page): changed text size on per device.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Calculator } from "@/components/calculator";
 const HomePage = () => (
   <main>
     <section className="flex min-h-screen items-center justify-center">
-      <div className="container flex max-w-md flex-col space-y-4 px-4">
-        <h1 className="text-center text-xl font-bold uppercase">
+      <div className="container flex max-w-lg flex-col space-y-6 px-4">
+        <h1 className="text-center text-xl font-bold uppercase sm:text-2xl md:text-3xl">
           Trade Size Calculator
         </h1>
         <Calculator />
