be6c7fa7a5ce2c41c70e4a6d6943d360cfc6de58
Roby Codes <robert.cfx@proton.me>
2024-10-25 02:17:38 +0200

    style(component): completed calculator layout.

--- src/components/calculator/index.tsx
+++ src/components/calculator/index.tsx
@@ -3,22 +3,26 @@ import { Forex } from "@/components/calculator/forex";
 import { Crypto } from "@/components/calculator/crypto";
 
 export const Calculator = () => (
-  <div className="rounded-md bg-white p-6 shadow-sm">
-    <Tabs defaultValue="forex" className="w-full">
-      <TabsList className="w-full">
-        <TabsTrigger value="forex" className="flex-1 uppercase">
-          Forex
-        </TabsTrigger>
-        <TabsTrigger value="crypto" className="flex-1 uppercase">
-          Crypto
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent value="forex">
-        <Forex />
-      </TabsContent>
-      <TabsContent value="crypto">
-        <Crypto />
-      </TabsContent>
+  <div className="rounded-md bg-white shadow-sm">
+    <Tabs defaultValue="forex" className="relative w-full">
+      <div className="border-b border-slate-100 p-6">
+        <TabsList className="w-full">
+          <TabsTrigger value="forex" className="flex-1 uppercase">
+            Forex
+          </TabsTrigger>
+          <TabsTrigger value="crypto" className="flex-1 uppercase">
+            Crypto
+          </TabsTrigger>
+        </TabsList>
+      </div>
+      <div className="p-6">
+        <TabsContent value="forex" className="mt-0 pt-0">
+          <Forex />
+        </TabsContent>
+        <TabsContent value="crypto" className="mt-0 pt-0">
+          <Crypto />
+        </TabsContent>
+      </div>
     </Tabs>
   </div>
 );
