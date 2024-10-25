b41d130d177bc6175557633935fd89822bdeb111
Roby Codes <robert.cfx@proton.me>
2024-10-25 03:16:49 +0200

    feat(component): created forex tab calculator.

---
+++ src/components/calculator/forex.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { calculateSize } from "@/lib/utils";
+import { ChangeEvent, useState } from "react";
+
+export const Forex = () => {
+  const [inputList, setInputList] = useState<TradeInputElements>({
+    capitalToRisk: 0,
+    entryPrice: 0,
+    stopLossPrice: 0,
+    positionSize: 0,
+  });
+
+  const handleInputUpdate = (event: ChangeEvent<HTMLInputElement>) => {
+    const newInputList = { ...inputList };
+    const { name, value } = event.target;
+    newInputList[name as keyof TradeInputElements] = Number(value);
+    setInputList({
+      ...newInputList,
+      positionSize: calculateSize(newInputList),
+    });
+  };
+
+  return (
+    <>
+      <div className="flex flex-col space-y-3 border-b border-slate-100 p-6">
+        <div className="flex flex-col space-y-1">
+          <Label
+            htmlFor="capitalToRisk"
+            className="text-sm font-semibold uppercase sm:text-base"
+          >
+            Capital To Risk
+          </Label>
+          <Input
+            id="capitalToRisk"
+            name="capitalToRisk"
+            className="appearance-none focus:outline-none focus:ring-0 focus:ring-offset-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
+            value={inputList.capitalToRisk}
+            type="number"
+            onChange={handleInputUpdate}
+          />
+        </div>
+        <div className="flex space-x-2">
+          <div className="flex flex-1 flex-col space-y-1">
+            <Label
+              htmlFor="entryPrice"
+              className="text-sm font-semibold uppercase sm:text-base"
+            >
+              Entry Price
+            </Label>
+            <Input
+              name="entryPrice"
+              id="entryPrice"
+              className="appearance-none focus:outline-none focus:ring-0 focus:ring-offset-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
+              value={inputList.entryPrice}
+              type="number"
+              onChange={handleInputUpdate}
+            />
+          </div>
+          <div className="flex flex-1 flex-col space-y-1">
+            <Label
+              htmlFor="stopLossPrice"
+              className="text-sm font-semibold uppercase sm:text-base"
+            >
+              Stop Loss Price
+            </Label>
+            <Input
+              name="stopLossPrice"
+              id="stopLossPrice"
+              className="appearance-none focus:outline-none focus:ring-0 focus:ring-offset-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
+              value={inputList.stopLossPrice}
+              type="number"
+              onChange={handleInputUpdate}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col space-y-3 p-6">
+        <div className="flex flex-col space-y-1">
+          <Label
+            htmlFor="capitalToRisk"
+            className="text-sm font-semibold uppercase sm:text-base"
+          >
+            Trade Lot Size
+          </Label>
+          <Input
+            id="capitalToRisk"
+            name="capitalToRisk"
+            className="appearance-none focus:outline-none focus:ring-0 focus:ring-offset-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
+            value={Math.abs(inputList.positionSize / 100000).toFixed(2)}
+            type="number"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
