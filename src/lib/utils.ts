c48d9651d9f30966e7c19108cd367ced1e6342b6
Roby Codes <robert.cfx@proton.me>
2024-10-25 03:17:06 +0200

    feat(utils): created calculateSize function.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,21 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function calculateSize(inputs: TradeInputElements) {
+  const { capitalToRisk, entryPrice, stopLossPrice } = inputs;
+
+  if (
+    capitalToRisk > 0 &&
+    entryPrice > 0 &&
+    stopLossPrice > 0 &&
+    entryPrice !== stopLossPrice
+  ) {
+    return capitalToRisk / (entryPrice - stopLossPrice);
+  }
+
+  return 0;
 }
